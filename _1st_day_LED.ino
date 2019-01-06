9b2433bf246a7245479a8d6297b304c157d48273
Moises Barbera Ramos <46381012+MoisesBarbera@users.noreply.github.com>
2019-01-06 15:00:56 +0000

    Add files via upload
    Simple method to represent the traffic lights colour change for non-experienced students.

---
+++ _1st_day_LED.ino
@@ -0,0 +1,40 @@
+int red = 13;
+int yellow = 12;
+int green = 11;
+
+void setup()
+{
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+}
+
+void loop()
+{
+  //red
+  digitalWrite(red, HIGH);
+  delay(5000); 
+
+  //red and yellow
+  digitalWrite(yellow, HIGH);
+  delay(2000); 
+
+  //green
+  digitalWrite(red, LOW);
+  digitalWrite(yellow, LOW);
+  digitalWrite(green, HIGH);
+  delay(5000);
+
+  //yellow
+  digitalWrite(green, LOW);
+  digitalWrite(yellow, HIGH);
+  delay(1000);
+
+  
+  
+  
+  
+}
+
+
+
